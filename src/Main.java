51f470cc781c9aaefb3ad6c5f7d635abb4fad305
Evgeniy Leschenko <melbourne333@mail.ru>
2023-03-09 16:30:43 +1000

    1.1.1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("В этой ветке пусто");
+        int test = 2;
+        int[][] array = new int[5][6];
     }
 }
