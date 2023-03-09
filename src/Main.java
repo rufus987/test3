c356f9a45a2945706b77a0dac4afffdb7fbd0b88
Evgeniy Leschenko <melbourne333@mail.ru>
2023-03-09 13:46:43 +1000

    Особая новая ветка

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        try {
-            int nextInt = sc.nextInt();
-            System.out.println(nextInt);
-        } catch (Exception exception){
-            exception.printStackTrace();
-        } finally {
-            System.out.println("OK");
-        }
-
-        System.out.println("Мы обработали ошибку");
-        System.out.println("Программа продолжается");
+        System.out.println("В этой ветке пусто");
     }
 }
